91a4969bf264be6a1e0f493757fee185b55947eb
Alina Slobodeniuk <112395038+AlinaSlobodeniuk@users.noreply.github.com>
2025-03-09 17:32:40 +0200

    task-1

---
+++ js/task-1.js
@@ -0,0 +1,15 @@
+const categories = document.querySelectorAll("#categories");
+const els = document.querySelectorAll(".item");
+
+console.log(`Numbers of categories: ${els.length}`);
+
+els.forEach(el => {
+
+    const title = el.querySelector("h2").textContent;
+
+    const elements = el.querySelectorAll("li").length;
+
+    console.log(`Category: ${title}`);
+    console.log(`Elements: ${elements}`);
+    
+});
