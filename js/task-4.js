36ceedb16fa3cfc1750efc4529eaa0a56e5a9efb
Alina Slobodeniuk <112395038+AlinaSlobodeniuk@users.noreply.github.com>
2025-03-10 01:59:13 +0200

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,20 @@
+const registerForm = document.querySelector(".login-form");
 
+registerForm.addEventListener("submit", sendSubmit);
+
+function sendSubmit(event) {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.elements.email.value.trim();
+    const password = form.elements.password.value.trim();
+    
+    if (email === "" || password === "") {
+      return console.log("All form fields must be filled in");
+    }
+
+    const str = `Email: ${email}, Password: ${password}`;
+    const result = str.split(",").reduce
+  
+    console.log(`Email: ${email}, Password: ${password}`);
+    form.reset();
+}
